2ae131ab68b828766872736e11247e73a493c483
tonon <salvio.tonon@gmail.com>
2026-01-28 18:55:26 -0300

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-  apps: [
-    {
-      name: "dev",
-      script: "npm",
-      args: "run dev:start",
-      watch: false,        
-      autorestart: true,   
-      max_restarts: 5,
-      log_file: "./logs/combined.log", 
-      out_file: "./logs/out.log",      
-      error_file: "./logs/error.log",  
-      merge_logs: true,
-      env: {
-        NODE_ENV: "development",
-        PORT: 8080
-      }
+ apps: [
+  {
+    name: "dev",
+    script: "npm",
+    args: "run dev:start",
+    instances: 1,    
+    watch: false,
+    autorestart: true,
+    max_restarts: 5,
+    log_file: "./logs/combined.log",
+    out_file: "./logs/out.log",
+    error_file: "./logs/error.log",
+    merge_logs: true,
+    env: {
+      NODE_ENV: "development",
+      PORT: 8080
     }
-  ]
+  }
+]
 };
