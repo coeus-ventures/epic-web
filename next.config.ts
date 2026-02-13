47f820096b7fe31970aeb9b434935bab131955f1
tonon <salvio.tonon@gmail.com>
2026-02-13 17:26:51 -0300

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["https://*.http.cloud.morph.so"],
+  allowedDevOrigins: ["*.http.cloud.morph.so"],
 };
 
 export default nextConfig;
