8752a641cc56cf3432284fa596cc20365bb57064
tonon <salvio.tonon@gmail.com>
2026-02-17 19:47:46 -0300

    move to layout

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,17 +1,13 @@
 import { Suspense } from "react";
 import { AdminDashboard } from "./components/admin-dashboard";
 import { AdminDashboardSkeleton } from "./components/admin-dashboard-skeleton";
-import { IframeAuthNotifier } from "./behaviors/iframe-auth-notifier/iframe-auth-notifier";
 import { getAdminStats } from "./behaviors/admin-stats/admin-stats.action";
 
 export default function AdminPage() {
   return (
-    <>
-      <IframeAuthNotifier />
-      <Suspense fallback={<AdminDashboardSkeleton />}>
-        <AdminDashboardContent />
-      </Suspense>
-    </>
+    <Suspense fallback={<AdminDashboardSkeleton />}>
+      <AdminDashboardContent />
+    </Suspense>
   );
 }
 
