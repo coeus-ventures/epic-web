4b3a91cffa95f9f2ee91fb13465bd861d43e6d2a
Edmar Ferreira <edmaroferreira@gmail.com>
2026-03-12 15:46:48 -0700

    remove dev indicator

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  devIndicators: false,
   allowedDevOrigins: ["*.http.cloud.morph.so", "*.proxy.epic.new", "*.lvh.me"],
 };
 
